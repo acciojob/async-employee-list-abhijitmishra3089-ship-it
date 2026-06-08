849cb2aad2d4e9c3a5c55b9c29b46b8fa0c61e42
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-08 19:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ async function getEmployees(){
 	try {
 	const response= await fetch('employees.json');
 	const data=await response.json();
-		data.sort((a,b)=>a.age-b.age)
+	data.sort((a,b)=>a.age-b.age)
 	console.log(data)
 	}catch (err) {
 		console.error(err)
-	
+	}
 }
 	getEmployees();
