1c16e547c028f6157e7158c615b43f5908013951
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-08 19:54:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+async function getEmployees(){
+	try {
+	const response= await fetch('employees.json');
+	const data=await response.json();
+	console.log(data)
+	}catch (err) {
+		console.error(err)
+	
+}
+	getEmployees();
